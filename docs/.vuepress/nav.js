23b808ad4582904ddfc80c9dd5c2be09ffe045f3
wangbeijing <2231101379@qq.com>
2026-03-17 09:38:38 +0800

    增加Agent最小demo

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,8 +1,8 @@
 module.exports = [
   { text: '首页', link: '/' },
   {
-    text: 'Transformer',
-    link: '/Transformer/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    text: 'HelloAgent',
+    link: '/HelloAgent/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
   },
   {
     text: '提示词工程',
