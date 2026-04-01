e3e04227d056030faed52fb4e0c9af4bc4263f1a
wangbeijing <2231101379@qq.com>
2026-04-01 11:10:50 +0800

    增加fastAPI

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -5,6 +5,10 @@ module.exports = [
     link: '/HelloAgent/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
   },
   {
+    text: 'FastAPI',
+    link: '/FastAPI/',
+  },
+  {
     text: '提示词工程',
     link: '/promptEngineer/',
     items: [
