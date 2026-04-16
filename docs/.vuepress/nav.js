9bdf82ee3eebe180a472b44d2de717eb56350080
wangbeijing <2231101379@qq.com>
2026-04-16 21:38:17 +0800

    增加项目笔记

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -39,8 +39,8 @@ module.exports = [
     link: '/HelloAgent/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
   },
   {
-    text: 'FastAPI',
-    link: '/FastAPI/',
+    text: 'ProjectDesc',
+    link: '/ProjectDesc/',
   },
   {
     text: '知识复盘',
