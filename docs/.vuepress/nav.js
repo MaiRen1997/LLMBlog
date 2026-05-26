95da194c1cf4d4e34f42905c57cfa828bcd1cc5c
wangbeijing <2231101379@qq.com>
2026-05-26 14:02:20 +0800

    修改链接

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -1,17 +1,11 @@
 module.exports = [
   { text: '首页', link: '/' },
-  {
-    text: '提示词工程',
-    link: '/promptEngineer/',
-    items: [
-      // { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-      // { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-      // { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-      // { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
-    ],
-  },
   { text: '基本使用', link: '/' ,
     items: [
+      {
+        text: '提示词工程',
+        link: '/promptEngineer/',
+      },
       {
         text: 'RAG',
         link: '/RAG/',
@@ -24,6 +18,18 @@ module.exports = [
         text: 'Agent&工作流',
         link: '/Agent/',
       },
+      {
+        text: '微调',
+        link: '/Fine-tuning/',
+      },
+      {
+        text: '多模态',
+        link: '/Multimodal/',
+      },
+      {
+        text: '私有化部署',
+        link: '/PrivateDeploy/',
+      },
     ]
   },
   {
@@ -33,18 +39,6 @@ module.exports = [
   {
     text: '知识复盘',
     link: '/reviewKnnowledge/',
-  },
-  {
-    text: '微调',
-    link: '/Fine-tuning/',
-  },
-  {
-    text: '多模态',
-    link: '/Multimodal/',
-  },
-  {
-    text: '私有化部署',
-    link: '/PrivateDeploy/',
   },
   {
     text: 'HelloAgent',
